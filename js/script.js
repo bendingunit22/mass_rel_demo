e634dc844361a1566c85855ff36e1f1cc195c447
Michael Raimi <raimi.michael@gmail.com>
2013-04-26 19:07:43 -0400

    final

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function() {
         return '<table>' + band.name  + '<span class="afterBand"><span class="red">+'+ band.count  + '</span><span class="ment"> mentions </span></span></table>' ;
       }).forEach(function(tag) {
         $('ul#bands').append(tag).effect('highlight', {color: 'rgba(222,22,22,.2)'}, 60);
-      });
+      });                             //flashing red for refresh
     });
     poller.start();  
     
